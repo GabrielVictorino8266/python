e73a5ccfc2a1d0d5add0fd0f1a12b8f96258b640
Gabriel_Victorino <72820281+GabrielVictorino8266@users.noreply.github.com>
2022-05-15 20:17:49 -0300

    Update hello.py

--- hello-world/hello.py
+++ hello-world/hello.py
@@ -1,3 +1,4 @@
 print('Hello, World!')
 print('This is my firt program on Python!')
 print('Este é o meu primeiro programa em Python!')
+print('Tutorial da Microsoft sobre como Instalar Python no VsCode.')
