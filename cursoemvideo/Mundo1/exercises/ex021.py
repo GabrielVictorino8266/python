4fc1754552c09c8c6e90aad622e913febd46275c
Gabriel_Victorino <72820281+GabrielVictorino8266@users.noreply.github.com>
2022-12-17 01:05:20 -0300

    Update Exercises Play Song

--- cursoemvideo/Mundo1/exercises/ex021.py
+++ cursoemvideo/Mundo1/exercises/ex021.py
@@ -3,9 +3,33 @@
 # Import the modules
 
 import pygame
+from pygame import mixer
 
-pygame.mixer.init()
-pygame.mixer.music.load('ex021.mp3')
-pygame.mixer.music.play()
-pygame.event.wait()
+# Instancia mixer
+mixer.init()
 
+# Carrega um arquivo de música
+mixer.music.load('ex021.mp3')
+
+# Dá um play na música
+mixer.music.play()
+print("Tocando a música...")
+
+while True:
+    print("-----------------------------------------------")
+    print("Press 'p' to pause the music")
+    print("Press 'r' to resume the music")
+    print("Press 'e' to exit the program")
+
+    userInput = input(" ")
+
+    if userInput == 'p':
+
+        mixer.music.pause()
+        print('Música pausada.')
+    elif userInput == 'r':
+        mixer.music.unpause()
+        print("Música está tocando.")
+    elif userInput == 'e':
+        print("Você saiu do player")
+        break
