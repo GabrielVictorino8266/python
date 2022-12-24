1bbfa8958e745d7ac652bbf2b78fc4121256b629
Gabriel_Victorino <72820281+GabrielVictorino8266@users.noreply.github.com>
2022-12-24 15:55:32 -0300

    Update class09.py

--- cursoemvideo/Mundo1/classes/Aula 09/class09.py
+++ cursoemvideo/Mundo1/classes/Aula 09/class09.py
@@ -1,6 +1,13 @@
+print('''
+Hello, Today we'll learn 
+more about STRINGS.
+''')
+
 # Fatiamento
 
 frase = "Curso em Vídeo Python"
+
+print(frase)
 print(frase[9])
 
 # Output:
@@ -42,3 +49,53 @@ print(frase.count('o'))
 print(frase.count("o",0,14))
 
 # Continuar a partir de 16:10
+
+print(frase.find('deo'))
+# retorna a posição do texto expecificado no método find.
+
+print(frase.find('Android'))
+
+#Retorna -1, ou seja, NÃO ENCONTRADO
+
+print('Curso' in frase)
+# O output esperado é True
+
+
+
+# TRANSFORMAÇÃO
+
+print(frase.replace('Curso', 'Android'))
+# Substitui um texto da string, mas não sobescreve a variável.
+
+print('UPPER CASE: ' + frase.upper())
+# Retorna TUDO EM MAISÚCULO
+print('lower case: ' + frase.lower())
+# Retorna tudo em minúsculo.
+
+print("Captilize" + frase.capitalize())
+# Retorna apenas o primeiro caractere de uma string em Maiúsculo
+
+print(frase.title())
+# Deicxa a primeira letra de todas as palavras em MAIÚSCULA.
+
+frase2 = "  aprenda python sem espaços  "
+print(frase2)
+print(frase2.strip())
+
+print(frase2.rstrip())
+# Elimina apenas o espaço no final da string.
+
+print(frase2.lstrip())
+# Remove o espaço apenas da esquerda.
+
+
+# ****DIVISÃO****
+
+print(frase.split())
+# Cria uma divisão quando encontrar um espaço na string. 
+# Ele retorna um array (lista) das palavras separadas.
+
+frase3 = frase.split()
+print(' '.join(frase3))
+# Junta as papalvras separadas em listas e adiciona o caractere especificado anteriormente ao Join.
+
